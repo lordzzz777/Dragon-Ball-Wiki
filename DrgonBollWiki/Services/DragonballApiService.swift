f454a34a82c764e16fdf8b0ab0ac01d1bfe9bc3e
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2024-03-02 18:46:27 -0300

    Elimina referencia de archivos que se encontraban ocultos

--- DrgonBollWiki/Services/DragonballApiService.swift
+++ DrgonBollWiki/Services/DragonballApiService.swift
@@ -10,11 +10,6 @@ import Foundation
 final class DragonballAPIService {
     private let allCharactersEndPoint = "https://dragonball-api.com/api/characters?page=1&limit=58"
     
-    private enum ApiError: Error {
-        case invalidURL
-        case invalidResponse
-    }
-    
     ///Obtiene todos los personajes de Dragonball API
     /// - Returns: Una instancia de `Characters` la cual contiene toda la información básica de cada personaje
     func getCharacters() async throws -> Characters {
@@ -36,28 +31,10 @@ final class DragonballAPIService {
             throw error
         }
     }
-    
-    ///Obtiene un personaje correspondiente al id
-    /// - Parameters: un id de tipo `Int`
-    /// - Returns: Una instancia de `SingleCharacter` la cual tiene toda la información de un personaje
-    func getSingleCharcter(id: Int) async throws -> SingleCharacter {
-        do {
-            let singleCharacterURL = "https://dragonball-api.com/api/characters/\(id)"
-            guard let url = URL(string: singleCharacterURL) else {
-                throw ApiError.invalidURL
-            }
-            
-            let (data, response) = try await URLSession.shared.data(from: url)
-            
-            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                throw ApiError.invalidResponse
-            }
-            
-            let decoder = JSONDecoder()
-            decoder.keyDecodingStrategy = .convertFromSnakeCase
-            return try decoder.decode(SingleCharacter.self, from: data)
-        } catch {
-            throw error
-        }
-    }
+}
+
+
+enum ApiError: Error {
+    case invalidURL
+    case invalidResponse
 }
