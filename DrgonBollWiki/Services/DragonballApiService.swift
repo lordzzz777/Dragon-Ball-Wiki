7d05d6d7c01be80336e45711309d58c4d4942a77
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2024-03-02 18:52:18 -0300

    Agrega la lógica para traer toda la información de un solo personaje

--- DrgonBollWiki/Services/DragonballApiService.swift
+++ DrgonBollWiki/Services/DragonballApiService.swift
@@ -10,6 +10,12 @@ import Foundation
 final class DragonballAPIService {
     private let allCharactersEndPoint = "https://dragonball-api.com/api/characters?page=1&limit=58"
     
+    private enum ApiError: Error {
+        case invalidURL
+        case invalidResponse
+    }
+
+    
     ///Obtiene todos los personajes de Dragonball API
     /// - Returns: Una instancia de `Characters` la cual contiene toda la información básica de cada personaje
     func getCharacters() async throws -> Characters {
@@ -31,10 +37,28 @@ final class DragonballAPIService {
             throw error
         }
     }
+    
+    
+    func getSingleCharacter(id: Int) async throws -> SingleCharacter {
+        do {
+            let singleCharacterEndPoint = "https://dragonball-api.com/api/characters/\(id)"
+            guard let url = URL(string: singleCharacterEndPoint) else {
+                throw ApiError.invalidURL
+            }
+            
+            let (data, response) = try await URLSession.shared.data(from: url)
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                throw ApiError.invalidResponse
+            }
+            
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            return try decoder.decode(SingleCharacter.self, from: data)
+        } catch {
+            throw error
+        }
+    }
 }
 
 
-enum ApiError: Error {
-    case invalidURL
-    case invalidResponse
-}
