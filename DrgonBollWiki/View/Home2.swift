73c9d6b7d04f527ca8f49705f2a850ea072c179a
lordzzz777 <114311106+lordzzz777@users.noreply.github.com>
2024-03-03 16:39:05 +0100

    Actualización, se trae datos de API a las vista,
    Se añade ala clase "DragonballAPIService", el "ObservableObject", se crea una carpeta "FX_Styles", donde se introduce el archivo "ParallaxHeder" y "Card", en el archivo home se realiza varias modificaciones de prueba, y se crea una vista Home2, para probar de forma sencilla que es lo que esta mandado la api

---
+++ DrgonBollWiki/View/Home2.swift
@@ -0,0 +1,57 @@
+//
+//  Home2.swift
+//  DrgonBollWiki
+//
+//  Created by Esteban Perez Castillejo on 3/3/24.
+//
+
+import SwiftUI
+
+//Vista pra pribar que la API Funciona
+struct Home2: View {
+    @StateObject private var apiService = DragonballAPIService()
+    @State private var characters: Characters?
+    @State private var isLoading = false
+    
+    var body: some View {
+        NavigationStack{
+            VStack{
+                if isLoading{
+                    ProgressView()
+                }else if let characters = characters{
+                    List(characters.items, id:\.id){ character in
+                        VStack(alignment: .leading){
+                            AsyncImage(url: URL(string: character.image)) { image in
+                                image.resizable()
+                                    .aspectRatio(contentMode: .fit)
+                            } placeholder: {
+                                ProgressView()
+                            }
+                            .frame(width: 100, height: 100)
+                            Text(character.name).font(.title)
+                            Text(character.description)
+                        }
+                    }
+                }else{
+                    Text("No cherate available")
+                }
+            }
+        }.navigationTitle("Dragon Ball Charaters")
+            .onAppear{
+                isLoading = true
+                Task{
+                    do{
+                        characters = try await apiService.getCharacters()
+                        isLoading = false
+                    }catch{
+                        print("Error: \(error)")
+                        isLoading = false
+                    }
+                }
+            }
+    }
+}
+
+#Preview {
+    Home2()
+}
