9416880c959a3d7ca7a7e9127ed915007c5d661f
lordzzz777 <114311106+lordzzz777@users.noreply.github.com>
2024-03-04 03:50:54 +0100

    Auto stash before rebase of "origin/Development"

--- DrgonBollWiki/View/Home2.swift
+++ DrgonBollWiki/View/Home2.swift
@@ -10,9 +10,11 @@ import SwiftUI
 //Vista pra pribar que la API Funciona
 struct Home2: View {
     @State private var apiService = DragonballAPIService()
+    @State private var singleCharacter: SingleCharacter?
     @State private var characters: Characters?
     @State private var planetes: Planets?
     @State private var isLoading = false
+    @State private var showViewDatail = false
     
     var body: some View {
         NavigationStack{
@@ -20,8 +22,18 @@ struct Home2: View {
                 if isLoading{
                     ProgressView()
                 }else if let characters = characters{
+                    List(planetes?.items ?? [], id:\.id){ planete in
+                        Text(planete.name)
+                        AsyncImage(url: URL(string: planete.image)) { image in
+                            image.resizable()
+                                .aspectRatio(contentMode: .fit)
+                        } placeholder: {
+                            ProgressView()
+                        }.frame(width: 100, height: 100)
+                            .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+                    }
                     List(characters.items, id:\.id){ character in
-                      
+                        NavigationLink(destination: ViewDetail()){
                             VStack(alignment: .leading){
                                 AsyncImage(url: URL(string: character.image)) { image in
                                     image.resizable()
@@ -29,15 +41,17 @@ struct Home2: View {
                                 } placeholder: {
                                     ProgressView()
                                 }
-                        
-                            .frame(width: 100, height: 100)
-                            Text(character.name).font(.title)
-                            Text(character.description)
-                            Text(character.race)
-                            Text(character.affiliation)
-                            Text(character.gender)
-                            Text(character.ki)
-                            Text(character.maxKi)
+                                
+                                .frame(width: 100, height: 100)
+                                Text(character.name).font(.title)
+                                Text(character.description)
+                                Text(character.race)
+                                Text(character.affiliation)
+                                Text(character.gender)
+                                Text(character.ki)
+                                Text(character.maxKi)
+                            }
+                            
                         }
                     }
                 }else{
@@ -49,6 +63,7 @@ struct Home2: View {
                 isLoading = true
                 Task{
                     do{
+                        planetes = try await apiService.getPlanets()
                         characters = try await apiService.getCharacters()
                         isLoading = false
                     }catch{
---
+++ DrgonBollWiki/View/ViewDetail.swift
@@ -0,0 +1,18 @@
+//
+//  ViewDetail.swift
+//  DrgonBollWiki
+//
+//  Created by Esteban Perez Castillejo on 4/3/24.
+//
+
+import SwiftUI
+
+struct ViewDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewDetail()
+}
