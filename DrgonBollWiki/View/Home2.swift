b2ea6605b41ae7a4bfbd4928b2b6f6555f807e1b
lordzzz777 <114311106+lordzzz777@users.noreply.github.com>
2024-03-04 04:37:44 +0100

    actualizada, Vista Home2

--- DrgonBollWiki/View/Home2.swift
+++ DrgonBollWiki/View/Home2.swift
@@ -9,35 +9,19 @@ import SwiftUI
 
 //Vista pra pribar que la API Funciona
 struct Home2: View {
-<<<<<<< Updated upstream
-    @State private var apiService = DragonballAPIService()
-    @State private var singleCharacter: SingleCharacter?
-=======
-    @State private var apiService: HomeViewModel
->>>>>>> Stashed changes
-    @State private var characters: Characters?
+    @State private var homeViewModel = HomeViewModel(allCaractersDataService: AllCharactersDataService())
+    @State private var selectedCharacter: Character?
+    
     @State private var planetes: Planets?
     @State private var isLoading = false
-    @State private var showViewDatail = false
     
     var body: some View {
         NavigationStack{
             VStack{
                 if isLoading{
                     ProgressView()
-                }else if let characters = characters{
-                    List(planetes?.items ?? [], id:\.id){ planete in
-                        Text(planete.name)
-                        AsyncImage(url: URL(string: planete.image)) { image in
-                            image.resizable()
-                                .aspectRatio(contentMode: .fit)
-                        } placeholder: {
-                            ProgressView()
-                        }.frame(width: 100, height: 100)
-                            .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
-                    }
+                }else if let characters = homeViewModel.allCharacters {
                     List(characters.items, id:\.id){ character in
-                        NavigationLink(destination: ViewDetail()){
                             VStack(alignment: .leading){
                                 AsyncImage(url: URL(string: character.image)) { image in
                                     image.resizable()
@@ -45,11 +29,27 @@ struct Home2: View {
                                 } placeholder: {
                                     ProgressView()
                                 }
-                                
-                                .frame(width: 100, height: 100)
-                                Text(character.name).font(.title)
-                                Text(character.description)
-                                Text(character.race)
+                            .frame(width: 100, height: 100)
+                            Text(character.name).font(.title)
+                            Text(character.description)
+                            Text(character.race)
+                            Text(character.affiliation)
+                            Text(character.gender)
+                            Text(character.ki)
+                            Text(character.maxKi)
+                        }
+                    }
+                }else{
+                    Text("No cherate available")
+                }
+            }
+        }.navigationTitle("Dragon Ball Charaters")
+    }
+}
+
+#Preview {
+    Home2()
+}
                                 Text(character.affiliation)
                                 Text(character.gender)
                                 Text(character.ki)
