2188918ab2a6db56f9202d1c2af5f750588f09f7
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2024-03-13 12:38:26 -0300

    Agrega card con información básica

---
+++ DrgonBollWiki/View/AllCharactersView.swift
@@ -0,0 +1,18 @@
+//
+//  AllCharactersView.swift
+//  DrgonBollWiki
+//
+//  Created by Jacob Aguilar on 13-03-24.
+//
+
+import SwiftUI
+
+struct AllCharactersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AllCharactersView()
+}
---
+++ DrgonBollWiki/View/CharacterDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  CharacterDetailView.swift
+//  DrgonBollWiki
+//
+//  Created by Jacob Aguilar on 13-03-24.
+//
+
+import SwiftUI
+
+struct CharacterDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CharacterDetailView()
+}
