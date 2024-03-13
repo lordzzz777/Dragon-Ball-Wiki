fd8cbd975343619ef2c89f02c2473b94d9ba1b96
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2024-03-13 19:30:21 -0300

    Mejora la vista de character detail view

--- DrgonBollWiki/View/CharacterDetailView.swift
+++ DrgonBollWiki/View/CharacterDetailView.swift
@@ -11,10 +11,12 @@ struct CharacterDetailView: View {
     
     @State private var singleCharacterViewModel: SingleCharacterViewModel
     @State private var idTranformation: Int = 0
+    @Binding var selectedCharacterKiColor: Color
     let selectedCharacter: Character
     
-    init(singleCharacterViewModel: SingleCharacterProtocol, selectedCharacter: Character) {
+    init(singleCharacterViewModel: SingleCharacterProtocol, selectedCharacter: Character, selectedCharacterKiColor: Binding<Color>) {
         _singleCharacterViewModel = State(wrappedValue: SingleCharacterViewModel(singleCharacterDataService: singleCharacterViewModel))
+        _selectedCharacterKiColor = selectedCharacterKiColor
         self.selectedCharacter = selectedCharacter
     }
     
@@ -28,11 +30,16 @@ struct CharacterDetailView: View {
                 } placeholder: {
                     ProgressView()
                 }
-                .frame(height: 490).shadow(color: .orange , radius: 15, x: 0, y: 0 ).padding(.top, 10)
+                .frame(height: 490)
+                .shadow(color: selectedCharacterKiColor , radius: 15, x: 0, y: 0 )
+                .padding(.top, 10)
                 
                 VStack {
                     Text("\(selectedCharacter.name)")
-                        .font(.custom("SaiyanSans", size: 40)).shadow(color: .blue , radius: 15, x: 0, y: 0 )
+                        .font(.custom("SaiyanSans", size: 42))
+                        .foregroundStyle(Color.yellow)
+                        .shadow(color: .black, radius: 0, x: 1, y: 1)
+                        .shadow(color: .white, radius: 0, x: -1, y: -1)
                 }
                 .frame(maxWidth: .infinity, alignment: .leading)
                 
@@ -42,14 +49,20 @@ struct CharacterDetailView: View {
                             Text("Ki")
                             
                             Text("\(selectedCharacter.ki)")
+                                .font(.body)
+                                .fontWeight(.bold)
                         }
                         
                         Spacer()
                         
                         VStack(alignment: .trailing) {
                             Text("\(selectedCharacter.race)")
+                                .font(.custom("SaiyanSans", size: 25))
+                                .foregroundStyle(selectedCharacterKiColor)
                             
                             Text("\(selectedCharacter.affiliation)")
+                                .font(.custom("SaiyanSans", size: 25))
+                                .foregroundStyle(Color.red)
                         }
                     }
                 }
@@ -59,6 +72,7 @@ struct CharacterDetailView: View {
                 if let transformation = singleCharacterViewModel.character?.transformations {
                     if transformation.count > 0 {
                         Text("Transformaciones ")
+                            .fontWeight(.bold)
                             .frame(maxWidth: .infinity, alignment: .leading)
                     }
                 }
@@ -109,6 +123,6 @@ struct CharacterDetailView: View {
 #Preview {
     let selectedCharacter = Character(id: 1, name: "Goku", ki: "60.000.000", maxKi: "90 Septillion", race: "Saiyan", gender: "Male", description: "El protagonista de la serie, conocido por su gran poder y personalidad amigable. Originalmente enviado a la Tierra como un infante volador con la misión de conquistarla. Sin embargo, el caer por un barranco le proporcionó un brutal golpe que si bien casi lo mata, este alteró su memoria y anuló todos los instintos violentos de su especie, lo que lo hizo crecer con un corazón puro y bondadoso, pero conservando todos los poderes de su raza. No obstante, en la nueva continuidad de Dragon Ball se establece que él fue enviado por sus padres a la Tierra con el objetivo de sobrevivir a toda costa a la destrucción de su planeta por parte de Freeza. Más tarde, Kakarot, ahora conocido como Son Goku, se convertiría en el príncipe consorte del monte Fry-pan y líder de los Guerreros Z, así como el mayor defensor de la Tierra y del Universo 7, logrando mantenerlos a salvo de la destrucción en innumerables ocasiones, a pesar de no considerarse a sí mismo como un héroe o salvador.", image: "https://res.cloudinary.com/dgtgbyo76/image/upload/v1699044374/hlpy6q013uw3itl5jzic.webp", affiliation: "Z Fighter", deletedAt: nil)
     
-    return CharacterDetailView(singleCharacterViewModel: MockSingleCharacterDataService(testData: nil), selectedCharacter: selectedCharacter)
+    return CharacterDetailView(singleCharacterViewModel: MockSingleCharacterDataService(testData: nil), selectedCharacter: selectedCharacter, selectedCharacterKiColor: .constant(Color.yellow))
         .preferredColorScheme(.dark)
 }
